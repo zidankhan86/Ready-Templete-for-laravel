996c8ab4be2a4f17412928be37143405176bc12c
zidankhan86 <zidankhan718@gmail.com>
2025-02-21 13:05:12 +0600

    fix:...

--- resources/views/backend/components/fixed/header.blade.php
+++ resources/views/backend/components/fixed/header.blade.php
@@ -168,20 +168,13 @@
             <div class="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="./">
-                            <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                <!-- Download SVG icon from http://tabler.io/icons/icon/home -->
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                                    <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                                    <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                                    <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                                </svg>
+                        <a class="nav-link" href="{{route('dashboard')}}">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block" data-bs-toggle="tooltip" data-bs-placement="top" title="Dashboard">
+                                <x-backend.svg.home2-svg />
                             </span>
-                            <span class="nav-link-title">
+                            {{-- <span class="nav-link-title">
                                 Home
-                            </span>
+                            </span> --}}
                         </a>
                     </li>
                     <li class="nav-item dropdown">
